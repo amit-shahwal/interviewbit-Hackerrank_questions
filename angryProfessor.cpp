b99522e2703224fa708e383cbbe58f611db29671
amit-shahwal <amitshahwal24@gmail.com>
2020-06-11 22:20:44 +0530

    Thu, Jun 11, 2020 10:20:44 PM

---
+++ angryProfessor.cpp
@@ -0,0 +1,15 @@
+// Complete the angryProfessor function below.
+string angryProfessor(int k, vector<int> a)
+{
+
+    //stirng ss;
+    int count = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] <= 0)
+            count++;
+    }
+    if (count >= k)
+        return "NO";
+    return "YES";
+}
