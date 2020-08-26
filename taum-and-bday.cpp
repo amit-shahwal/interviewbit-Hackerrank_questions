88794905e13b1c8152e9ea1e3db36086d3ea1e90
amit-shahwal <amitshahwal24@gmail.com>
2020-08-26 12:13:38 +0530

    taum-and-bday.cpp

---
+++ taum-and-bday.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'taumBday' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER b
+ *  2. INTEGER w
+ *  3. INTEGER bc
+ *  4. INTEGER wc
+ *  5. INTEGER z
+ */
+
+long taumBday(int b, int w, int bc, int wc, int z) {
+return (long)w*(wc<bc+z?wc:bc+z)+(long)b*(bc<wc+z?bc:wc+z);
+}
